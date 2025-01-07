49251678a656549857aa51800d515458b284f7ee
abdo_alaraby <abdoalaraby74@outlook.com>
2025-01-07 18:06:34 +0200

    CodeForces - ICPC Assiut Sheets - Sheet 1 - A.Say Hello

---
+++ src/codeforces/icpc_assiut/sheet1/SayHello.java
@@ -0,0 +1,10 @@
+package codeforces.icpc_assiut.sheet1;
+
+import java.util.Scanner;
+
+public class SayHello {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hello, " + sc.nextLine());
+	}
+}
