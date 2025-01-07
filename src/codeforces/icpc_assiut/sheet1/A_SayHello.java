cbee6064c0ebde41d02f821f6705acc2b7d78826
abdo_alaraby <abdoalaraby74@outlook.com>
2025-01-07 18:07:40 +0200

    Renaming

--- src/codeforces/icpc_assiut/sheet1/A_SayHello.java
+++ src/codeforces/icpc_assiut/sheet1/A_SayHello.java
@@ -2,7 +2,7 @@ package codeforces.icpc_assiut.sheet1;
 
 import java.util.Scanner;
 
-public class SayHello {
+public class A_SayHello {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello, " + sc.nextLine());
