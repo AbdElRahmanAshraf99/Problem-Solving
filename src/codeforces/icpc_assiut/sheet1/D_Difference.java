ff2af08770eed033650eec8c494a48593a816289
abdo_alaraby <abdoalaraby74@outlook.com>
2025-01-07 21:57:25 +0200

    CodeForces - ICPC Assiut Sheets - Sheet 1 - D.Difference

---
+++ src/codeforces/icpc_assiut/sheet1/D_Difference.java
@@ -0,0 +1,10 @@
+package codeforces.icpc_assiut.sheet1;
+
+import java.util.Scanner;
+
+public class D_Difference {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Difference = " + ((in.nextLong() * in.nextLong()) - (in.nextLong() * in.nextLong())));
+	}
+}
