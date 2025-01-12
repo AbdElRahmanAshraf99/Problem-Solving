f2618c31d3f20f97ed0dd45509a2071bc23b8640
abdo_alaraby <abdoalaraby74@outlook.com>
2025-01-12 23:18:49 +0200

    CodeForces - ICPC Assiut Sheets - Sheet 2 - A.1 to N

---
+++ src/codeforces/icpc_assiut/sheet2/A_1ToN.java
@@ -0,0 +1,13 @@
+package codeforces.icpc_assiut.sheet2;
+
+import java.util.Scanner;
+
+public class A_1ToN {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.println(i + 1);
+		}
+	}
+}
